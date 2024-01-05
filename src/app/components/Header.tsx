918dbc099a9cda06762b30426632eaa546b5ce03
nacica <mikio.yokohama@gmail.com>
2024-01-05 10:13:31 +0900

    ログインページ修正

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,8 +2,13 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { useSession } from "next-auth/react";
+
 
 const Header = () => {
+  const {data:session}= useSession();
+  const user = session?.user;
+
   return (
     <header className="bg-slate-600 text-gray-100 shadow-lg">
       <nav className="flex items-center justify-between p-4">
@@ -29,7 +34,7 @@ const Header = () => {
               width={50}
               height={50}
               alt="profile_icon"
-              src={"/default_icon.png"}
+              src={user?.image || "/default_icon.png"}
             />
           </Link>
         </div>
