8a952788bd549b37c07e3035249088ec42a8d42a
nacica <mikio.yokohama@gmail.com>
2024-01-05 10:54:51 +0900

    vercel authentication設定変更

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -27,7 +27,7 @@ const Header = () => {
             className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
           >
             ログイン
-          </Link>
+          </Link> 
 
           <Link href={`/profile`}>
             <Image
