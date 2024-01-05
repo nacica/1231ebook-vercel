5caaa0dd46a6b54a1c9dafbc36ee9160b0cab6c0
nacica <mikio.yokohama@gmail.com>
2024-01-05 14:46:16 +0900

    microCMS修正

--- src/app/lib/microcms/client.ts
+++ src/app/lib/microcms/client.ts
@@ -7,7 +7,7 @@ export const client = createClient({
 
 export const getAllBooks = async () => {
     const allBooks = await client.get({
-      endpoint: "bookcommerce",
+      endpoint: "book-commerce",
       queries: {
         offset: 0,
         limit: 10,
