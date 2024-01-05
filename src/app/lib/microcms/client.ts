9582352078edea477e8a5a0b2b2606c2866b01ab
nacica <mikio.yokohama@gmail.com>
2024-01-05 13:30:46 +0900

    microCMS連係

--- src/app/lib/microcms/client.ts
+++ src/app/lib/microcms/client.ts
@@ -4,3 +4,15 @@ export const client = createClient({
   serviceDomain: process.env.NEXT_PUBLIC_SERVICE_DOMAIN!, 
   apiKey: process.env.NEXT_PUBLIC_API_KEY!
 });
+
+export const getAllBooks = async () => {
+    const allBooks = await client.get({
+      endpoint: "bookcommerce",
+      queries: {
+        offset: 0,
+        limit: 10,
+      },
+    });
+  
+    return allBooks;
+  };
