bf4b97bd599f26f059ebcf184e3e4f9d5474905d
nacica <mikio.yokohama@gmail.com>
2024-04-01 08:17:45 +0900

    mod

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -2,7 +2,7 @@ import Stripe from "stripe";
 import { NextResponse } from "next/server";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
-// const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
 
 export async function POST(request: Request, response: Response) {
   const { title, price, bookId, userId } = await request.json();
@@ -31,9 +31,9 @@ export async function POST(request: Request, response: Response) {
       mode: "payment",
       //  success_url: `http://localhost:3000/book/checkout-success?session_id={CHECKOUT_SESSION_ID}`,//この記述だと正常動作
              success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/book/checkout-success?session_id={CHECKOUT_SESSION_ID}`,
-             // success_url: `${baseUrl}/book/checkout-success?session_id={CHECKOUT_SESSION_ID}`,
+             
       cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
-            //   cancel_url: `${baseUrl}`,
+            
     });
     return NextResponse.json({
       checkout_url: session.url
