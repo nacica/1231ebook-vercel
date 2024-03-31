a5899618cbbc2d4b60d0d11f3f40f972dccd7eae
nacica <mikio.yokohama@gmail.com>
2024-03-31 21:39:18 +0900

    mod

--- src/app/book/checkout-success/page.tsx
+++ src/app/book/checkout-success/page.tsx
@@ -55,13 +55,14 @@ const PurchaseSuccess = () => {
           ご購入いただいた内容の詳細は、登録されたメールアドレスに送信されます。
         </p>
         <div className="mt-6 text-center">
-        
+        {bookUrl && (
           <Link
             href={`/book/${bookUrl}`}
             className="text-indigo-600 hover:text-indigo-800 transition duration-300"
           >
             購入した記事を読む
           </Link>
+           )}
         </div>
       </div>
     </div>
